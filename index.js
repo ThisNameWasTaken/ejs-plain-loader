adb8c4c0e72e74040756d1f6051bab0b7bf87708
Adrian <adriandinca2008@yahoo.com>
2018-11-05 17:40:35 +0200

    fix: Resolve all loader dependencies

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
+const { promisify } = require('util');
+const readFile = promisify(require('fs').readFile);
 const ejs = require('ejs');
 const { getOptions } = require('loader-utils');
 
-module.exports = function (source) {
+module.exports = function (source, map, meta) {
+    const callback = this.async();
+
     const options = Object.assign({
         filename: this.resourcePath,
         doctype: 'html',
         compileDebug: this.debug || false
     }, getOptions(this));
 
-    const template = ejs.compile(source, options);
-    template.dependencies.forEach(this.addDependency);
-    return template(options.data || {});
+    const compileEjs = async (source, cb) => {
+        try {
+            const template = ejs.compile(source, options);
+
+            const addDependencies = dependency => {
+                this.addDependency(dependency);
+                return readFile(dependency, 'utf8').then(source => {
+                    const subTemplate = ejs.compile(source, Object.assign(options, { filename: dependency }));
+                    if (subTemplate.dependencies.length) {
+                        subTemplate.dependencies.forEach(this.addDependency);
+                        return subTemplate.dependencies.map(addDependencies);
+                    }
+                    return Promise.resolve();
+                }).catch(err => cb(err));
+            }
+
+            await Promise.all(template.dependencies.map(addDependencies));
+            cb(null, template(options.data || {}));
+        } catch (error) {
+            cb(error);
+        }
+    }
+
+    compileEjs(source, function (err, result) {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, result, map, meta);
+    });
 }
