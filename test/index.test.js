66f1ba1c30fc4850efd8e4684d71656ffc163a4e
Adrian <adriandinca2008@yahoo.com>
2018-11-28 11:01:00 +0200

    test: Include ejs partials

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,20 @@
 import path from 'path';
 import { getCompiledOutput, readFile } from './utils';
 
-test('Takes plain html as input and outputs it', async () => {
+test('takes plain html as input and outputs it', async () => {
+    const folder = 'plain-html';
     const [ejsOutput, htmlOutput] = await Promise.all([
-        getCompiledOutput('./fixtures/plain-html/index.ejs'),
-        readFile(path.resolve(__dirname, './fixtures/plain-html/index.html'), 'utf8')
+        getCompiledOutput(`./fixtures/${folder}/index.ejs`),
+        readFile(path.resolve(__dirname, `./fixtures/${folder}/index.html`), 'utf8')
+    ]);
+    expect(ejsOutput).toBe(htmlOutput);
+});
+
+test('includes ejs partials', async () => {
+    const folder = 'include-partials';
+    const [ejsOutput, htmlOutput] = await Promise.all([
+        getCompiledOutput(`./fixtures/${folder}/index.ejs`),
+        readFile(path.resolve(__dirname, `./fixtures/${folder}/index.html`), 'utf8')
     ]);
     expect(ejsOutput).toBe(htmlOutput);
 });
