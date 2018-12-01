17e0a8fdf9d30267c44c28733037e2e5b3df6970
Adrian <adriandinca2008@yahoo.com>
2018-12-01 11:48:10 +0200

    refactor: Use a function to get the ejs and html outputs

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,21 @@
 import path from 'path';
 import { getCompiledOutput, readFile } from './utils';
 
+/**
+ * @param {string} folder - The folder inside the fixtures folder to get the output from
+ * @returns {Promise<[string, string]>} - EJS output and HTML output respectively
+ */
+const getOutputs = folder => Promise.all([
+    getCompiledOutput(`./fixtures/${folder}/index.ejs`),
+    readFile(path.resolve(__dirname, `./fixtures/${folder}/index.html`), 'utf8')
+]);
+
 test('takes plain html as input and outputs it', async () => {
-    const folder = 'plain-html';
-    const [ejsOutput, htmlOutput] = await Promise.all([
-        getCompiledOutput(`./fixtures/${folder}/index.ejs`),
-        readFile(path.resolve(__dirname, `./fixtures/${folder}/index.html`), 'utf8')
-    ]);
+    const [ejsOutput, htmlOutput] = await getOutputs('plain-html');
     expect(ejsOutput).toBe(htmlOutput);
 });
 
 test('includes ejs partials', async () => {
-    const folder = 'include-partials';
-    const [ejsOutput, htmlOutput] = await Promise.all([
-        getCompiledOutput(`./fixtures/${folder}/index.ejs`),
-        readFile(path.resolve(__dirname, `./fixtures/${folder}/index.html`), 'utf8')
-    ]);
+    const [ejsOutput, htmlOutput] = await getOutputs('include-partials');
     expect(ejsOutput).toBe(htmlOutput);
 });
